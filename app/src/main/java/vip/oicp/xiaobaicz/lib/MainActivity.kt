08821094e1ac3cc248484192a57f8b606c567d7f
laobocheng <bocheng.lao@viu.com>
2023-11-15 10:22:05 +0800

    ViewBinding

--- app/src/main/java/vip/oicp/xiaobaicz/lib/MainActivity.kt
+++ app/src/main/java/vip/oicp/xiaobaicz/lib/MainActivity.kt
@@ -3,11 +3,15 @@ package vip.oicp.xiaobaicz.lib
 import android.os.Bundle
 import vip.oicp.xiaobaicz.lib.common.app.AppCompatActivity
 import vip.oicp.xiaobaicz.lib.common.provider.ContextProvider
+import vip.oicp.xiaobaicz.lib.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val bind by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(bind.root)
         println(ContextProvider.applicationContext)
         println(ContextProvider.topActivityContext)
     }
