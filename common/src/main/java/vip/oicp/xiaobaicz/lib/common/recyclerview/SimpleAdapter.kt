068e31b08d9217c96ebb6f8f8786306995c09e74
laobocheng <bocheng.lao@viu.com>
2023-11-28 14:15:18 +0800

    SimpleAdapter

---
+++ common/src/main/java/vip/oicp/xiaobaicz/lib/common/recyclerview/SimpleAdapter.kt
@@ -0,0 +1,25 @@
+@file:JvmName("RecyclerViewX")
+
+package vip.oicp.xiaobaicz.lib.common.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+fun <T : ViewBinding> RecyclerView.simpleAdapter(factory: BindingFactory<T>, onBindBinding: (T, Any, Int) -> Unit): SimpleAdapter<T> {
+    val adapter = SimpleAdapter(factory, onBindBinding)
+    this.adapter = adapter
+    return adapter
+}
+
+class SimpleAdapter<T : ViewBinding>(
+    private val factory: BindingFactory<T>,
+    private val onBindBinding: (T, Any, Int) -> Unit,
+) : BindingListAdapter<T>() {
+    override fun bindingFactory(viewType: Int): BindingFactory<T> {
+        return factory
+    }
+
+    override fun onBindBinding(bind: T, position: Int) {
+        onBindBinding(bind, data[position], position)
+    }
+}
