09600f9bc8652d511feb675da5653d9597e73d9f
laobocheng <bocheng.lao@viu.com>
2024-02-05 10:27:37 +0800

    RecyclerView扩展新增tryCast

--- common/src/main/java/io/github/xiaobaicz/common/recyclerview/OnBindingCreate.kt
+++ common/src/main/java/io/github/xiaobaicz/common/recyclerview/OnBindingCreate.kt
@@ -5,3 +5,7 @@ import androidx.viewbinding.ViewBinding
 fun interface OnBindingCreate<T : ViewBinding> {
     fun onBindingCreate(bind: T)
 }
+
+inline fun <reified T : ViewBinding> ViewBinding.tryCast(block: T.() -> Unit) {
+    if (this is T) block(this)
+}
